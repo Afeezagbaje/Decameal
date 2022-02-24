f74712cc59911850069e68c5b6147fc9c2b98748
Tosin Ayodele <devtosxn@gmail.com>
2022-02-24 10:56:42 +0100

    add tests for welcome prompt

---
+++ src/tests/Welcome.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Welcome from "../pages/welcome";
+
+afterEach(cleanup);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Welcome />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("Welcome", () => {
+  it("renders welcome message", () => {
+    const { getByText } = render(<Welcome />);
+    expect(getByText("Welcome to Decameal")).toBeInTheDocument();
+  });
+});
