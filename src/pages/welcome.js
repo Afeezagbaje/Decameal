49f6b526b9fa5fdafcb9c2c87c90249cc02c8806
PETRUSBLACK <45966588+PETRUSBLACK@users.noreply.github.com>
2022-02-28 18:45:17 +0100

    Notification

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,10 +1,15 @@
+import Notifications from "./notifications"
+import { Container } from "@material-ui/core";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+
+
+
 const Welcome = () => (
-    <div>
-        <h1>Welcome to Decameal</h1>
-        <p>
-        DecaMeal is a food management system that would enable Decadevs and Decagon employees to subscribe and unsubscribe from Decagon Meal. The platform will allow its users (Decadevs and/or Decagon staff) to contact/message the kitchen staff and vise-versa, have a weekly meal time table and an admin dashboard
-        </p>
-    </div>
+    <Container>
+        <Notifications>
+            <KeyboardArrowLeftIcon />
+        </Notifications>
+    </Container>
 );
 
 
