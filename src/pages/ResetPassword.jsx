883964e6e26a3e137b3bd7b5e794e00303842379
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-03-01 12:04:05 +0100

    integrate notification api :octocat:

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,46 +1,57 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
 import "./reset.css";
 
 const ResetPassword = () => {
   return (
-    <div className="container">
+    <Box className="container">
       <Box
         className="left-side"
         sx={{ display: { xs: "none", md: "block" }, marginRight: "5px" }}
-      >
-      </Box>
+      ></Box>
 
-      <Box className="right-side" sx={{}}>
-        <h1 className="title">Reset Password!</h1>
-        <div className="form_container">
+      <Box className="right-side">
+        <Typography
+          variant="h4"
+          sx={{ textAlign: "center", marginBottom: "3rem", fontWeight: "600" }}
+        >
+          Reset Password!
+        </Typography>
+        <Box className="form_container">
           <form className="reset-form">
-            <div className="password-container">
-              <input
+            <Box className="password-container">
+              <TextField
+                fullWidth
+                variant="outlined"
                 type="password"
                 className="password"
                 placeholder="Old Password"
               />
               <span className="show_password">show</span>
-            </div>
-            <div className="password-container">
-              <input
+            </Box>
+            <Box className="password-container">
+              <TextField
+                fullWidth
+                variant="outlined"
                 type="password"
                 className="password"
                 placeholder="New Password"
               />
               <span className="show_password">show</span>
-            </div>
-            <input
-              type="button"
+            </Box>
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              sx={{ backgroundColor: "#1c1c1c", height: "60px" }}
               className="submit_button"
-              name="submit"
-              value="Reset"
-            />
+            >
+              Reset
+            </Button>
           </form>
-        </div>
+        </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
