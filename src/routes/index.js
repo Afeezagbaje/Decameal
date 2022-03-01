120dde6f2e50d29a9194bacdd82fb261cd4d2642
Bankole Oyewole <66924805+bankoleoye@users.noreply.github.com>
2022-03-01 08:13:59 +0100

    I created the login page for users

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../pages/login_page/login'
+// import LoginScreen from '../pages/login_page/login_screen';
 import Welcome from '../pages/welcome';
 
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Welcome} />
-  </Switch>
+  <Routes>
+    <Route exact path="/" element={<Welcome />} />
+    <Route exact path="/login_screen" element={<Login />} />
+  </Routes>
 );
 
 export default App;
