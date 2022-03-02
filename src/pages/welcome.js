f77b1ba125510339dff8722cae8e7a26a96409b4
Sanmi-Owoka <owokaayoola@outlook.com>
2022-03-02 11:56:39 +0100

    fixing failing linter

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,14 +1,16 @@
 import Footer from '../component/footer/Footer';
 
 const Welcome = () => (
-    <div>
-        <h1>Welcome to Decameal</h1>
-        <p>
-        DecaMeal is a food management system that would enable Decadevs and Decagon employees to subscribe and unsubscribe from Decagon Meal. The platform will allow its users (Decadevs and/or Decagon staff) to contact/message the kitchen staff and vise-versa, have a weekly meal time table and an admin dashboard
-        </p>
-        <Footer />
-    </div>
+  <div>
+    <h1>Welcome to Decameal</h1>
+    <p>
+      DecaMeal is a food management system that would enable Decadevs and
+      Decagon employees to subscribe and unsubscribe from Decagon Meal. The
+      platform will allow its users (Decadevs and/or Decagon staff) to
+      contact/message the kitchen staff and vise-versa, have a weekly meal time
+      table and an admin dashboard
+    </p>
+  </div>
 );
-
 
 export default Welcome;
