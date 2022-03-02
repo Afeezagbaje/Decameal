035a0367411d9c3878c66c34945dace86a895635
Olatunde Sodiq <olatundesodiq@gmail.com>
2022-03-02 18:19:54 +0100

    fix hero test errors

--- src/tests/hero.test.js
+++ src/tests/hero.test.js
@@ -6,33 +6,30 @@ import TopHero from "../components/topHero";
 
 afterEach(cleanup);
 it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<TopHero />, div);
+  const div = document.createElement("div");
+  ReactDOM.render(<TopHero />, div);
 });
 
 it("renders with displaying", () => {
-    render(<TopHero Text1="Read" />);
-    const textOneElement = screen.getByText('Read')
-    expect(textOneElement).toBeInTheDocument
-})
+  render(<TopHero Text1="Read" />);
+  const textOneElement = screen.getByText("Read");
+  expect(textOneElement).toBeInTheDocument();
+});
 
+it("renders with displaying code", () => {
+  render(<TopHero Text2="Code" />);
+  const textTwoElement = screen.getByText("Code");
+  expect(textTwoElement).toBeInTheDocument();
+});
 
-it("renders with displaying", () => {
-    render(<TopHero Text2="Code" />);
-    const textTwoElement = screen.getByText('Code')
-    expect(textTwoElement).toBeInTheDocument
-})
-
-
-it("renders with displaying", () => {
-    render(<TopHero Text3="Water" />);
-    const textThreeElement = screen.getByText('Water')
-    expect(textThreeElement).toBeInTheDocument
-})
-
+it("renders with displaying Water", () => {
+  render(<TopHero Text3="Water" />);
+  const textThreeElement = screen.getByText("Water");
+  expect(textThreeElement).toBeInTheDocument();
+});
 
 it("renders with displaying", () => {
-    render(<TopHero Text4="Food" />);
-    const textFourElement = screen.getByText('Food')
-    expect(textFourElement).toBeInTheDocument
-})
+  render(<TopHero Text4="Food" />);
+  const textFourElement = screen.getByText("Food");
+  expect(textFourElement).toBeInTheDocument;
+});
