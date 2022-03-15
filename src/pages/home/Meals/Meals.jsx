4cf20df5aeab6128a18e3d2b85a1e33e06e7bf36
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2022-03-15 06:46:55 +0100

    made_apicall_on_homepage

--- src/pages/home/Meals/Meals.jsx
+++ src/pages/home/Meals/Meals.jsx
@@ -1,15 +1,17 @@
 import { Box, Container, Grid, Paper, Typography } from "@mui/material";
-
-import Afang from "../../../assets/img/Afang.png";
 import Cards from "../../../components/Cards/Cards";
-import Egusi from "../../../assets/img/Egusi.png";
 import ExploreButton from "./ExploreButton";
-import Jollof_image from "../../../assets/img/chicken.png";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { meals} from "../../../store/slices/meals";
 
 const Meals = () => {
+  const dispatch = useAppDispatch();
+  const mealList = useAppSelector((state) => state.meals);
+  useEffect(() => {
+    dispatch(meals());
+  }, [dispatch]);
   return (
-
     <Paper
       elevation={0}
       square
@@ -31,27 +33,17 @@ const Meals = () => {
       </Typography>
       <Container>
         <Grid container spacing={12}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Cards
-              image={Jollof_image}
-              title="Jollof Rice"
-              body="Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Cards
-              image={Egusi}
-              title="Egusi Soup & Eba"
-              body="Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Cards
-              image={Afang}
-              title="Afang & Semo"
-              body="Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque"
-            />
-          </Grid>
+          {mealList.data && mealList.data.meals
+            ? [mealList.data.meals[0], mealList.data.meals[1], mealList.data.meals[2]].map((meal, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Cards
+                    image={meal.cover_img}
+                    title={meal.title}
+                    body={meal.description}
+                  />
+                </Grid>
+              ))
+            : ""}
         </Grid>
       </Container>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
